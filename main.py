73ea35a7c8314a72074ac7311aaf7eb666891874
shantanu pradip pokale <shantanupokale009@gmail.com>
2024-06-19 16:40:15 +0530

    Day 24: ADD lines to pages

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="p", unit="mm", format="A4")
-pdf.set_auto_page_break(auto=False,margin=0)
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
@@ -13,10 +13,12 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
-    pdf.ln(265)
+    pdf.ln(266)
 
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
@@ -24,6 +26,8 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
         # Set the footer
         pdf.ln(277)
